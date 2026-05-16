8392f3e3cfff8e78aed52fc4500957ab96cf0123
Intrapreneurial AI <bhawandeepkaur2000@outlook.com>
2026-05-16 21:21:20 +0100

    Create chapter-5-flexbox.js

---
+++ frontend-developer/chapter-5-flexbox.js
@@ -0,0 +1,21 @@
+/**
+ * chapter 5 - flexbox
+ *
+ * license: mit
+ * author: @bhawandeep kaur
+ *
+ * powerful one dimensional layout
+ */
+
+console.log("=== chapter 5: flexbox ===");
+
+const flexcss = `
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+`;
+
+console.log("flexbox is very useful for aligning items");
