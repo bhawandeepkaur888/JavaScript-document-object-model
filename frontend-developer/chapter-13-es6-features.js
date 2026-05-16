16915707b9052c5e5fca52de73de872463c224d7
Intrapreneurial AI <bhawandeepkaur2000@outlook.com>
2026-05-16 21:23:24 +0100

    Create chapter-13-es6-features.js

---
+++ frontend-developer/chapter-13-es6-features.js
@@ -0,0 +1,13 @@
+/**
+ * chapter 13 - modern javascript es6+
+ *
+ * license: mit
+ * author: @bhawandeep kaur
+ *
+ * arrow functions, destructuring, modules
+ */
+
+console.log("=== chapter 13: modern javascript ===");
+
+const arrow = () => console.log("arrow function");
+const { name, age } = user;
