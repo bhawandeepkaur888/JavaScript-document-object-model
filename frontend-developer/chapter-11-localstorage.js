a7c937db3c0ed349ee70496199bb85dfba9bf3b5
Intrapreneurial AI <bhawandeepkaur2000@outlook.com>
2026-05-16 21:23:00 +0100

    Create chapter-11-localstorage.js

---
+++ frontend-developer/chapter-11-localstorage.js
@@ -0,0 +1,13 @@
+/**
+ * chapter 11 - localstorage
+ *
+ * license: mit
+ * author: @bhawandeep kaur
+ *
+ * saving data in browser
+ */
+
+console.log("=== chapter 11: localstorage ===");
+
+localstorage.setitem("username", "bhawandeep");
+console.log(localstorage.getitem("username"));
