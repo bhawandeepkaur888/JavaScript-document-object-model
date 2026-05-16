513072a12bf3e2c2c061ba2377e97bd5a89bcdd0
Intrapreneurial AI <bhawandeepkaur2000@outlook.com>
2026-05-16 20:51:31 +0100

    Create chapter-1-what-is-dom.js

---
+++ backend-developer/chapter-1-what-is-dom.js
@@ -0,0 +1,23 @@
+/**
+ * chapter 1 - what is dom
+ *
+ * license: mit
+ * author: @bhawandeep kaur
+ *
+ * understanding document object model
+ */
+
+console.log("=== chapter 1: what is dom ===");
+
+// dom is a tree structure of html document
+// we can access and modify it using javascript
+
+// basic dom properties
+console.log("document title:", document.title);
+console.log("document url:", document.url);
+console.log("document domain:", document.domain);
+
+// accessing root elements
+console.log("html root element:", document.documentelement);
+console.log("head element:", document.head);
+console.log("body element:", document.body);
