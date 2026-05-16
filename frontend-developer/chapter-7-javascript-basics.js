a590797aabf5608c7ea524526cbb7f681f812b5a
Intrapreneurial AI <bhawandeepkaur2000@outlook.com>
2026-05-16 21:22:06 +0100

    Create chapter-7-javascript-basics.js

---
+++ frontend-developer/chapter-7-javascript-basics.js
@@ -0,0 +1,14 @@
+/**
+ * chapter 7 - javascript basics
+ *
+ * license: mit
+ * author: @bhawandeep kaur
+ *
+ * core programming concepts
+ */
+
+console.log("=== chapter 7: javascript basics ===");
+
+let name = "bhawandeep kaur";
+
+console.log("variables, data types, loops and functions are foundation");
